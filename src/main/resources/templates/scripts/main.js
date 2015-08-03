82cfa644097df640b0e72c0dabc426d57c323bb1
nagisio <mhmsynasc@gmail.com>
2015-08-03 19:35:37 +0900

    indexのView実装
    新規投稿フォーム
    投稿一覧表示

--- src/main/resources/templates/scripts/main.js
+++ src/main/resources/templates/scripts/main.js
@@ -1,10 +1,15 @@
 ;(function($) {
   var _document = $(document);
 
-  _document.on('submit', '#post-content', function(event) {
+  _document.on('click', '.write-button', function() {
+    $('#posts').slideToggle(400);
+  })
+
+  _document.on('click', '#submit', function(event) {
     event.preventDefault();
 
     var jsondata = {
+      username: $('.post-content--username').val(),
       title: $('.post-content--title').val(),
       content: $('.post-content--text').val()
     };
@@ -15,7 +20,7 @@
       contentType: 'application/json',
       data: JSON.stringify(jsondata)
     })
-    .done(function() {
+    .done(function(data) {
       console.log("success");
     })
     .fail(function() {
