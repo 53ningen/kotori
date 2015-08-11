717b4cad0de1a210c0654f998e1db7d9568e0acf
nagisio <mhmsynasc@gmail.com>
2015-08-11 20:09:10 +0900

    Adminページにおける編集機能のView実装

--- src/main/resources/templates/scripts/kotori.admin.js
+++ src/main/resources/templates/scripts/kotori.admin.js
@@ -1,5 +1,68 @@
 ;(function($) {
   var _document = $(document);
+  var updatedata;
+
+  /**
+   * 編集ボタンをクリックした時のイベント
+   **/
+  _document.on('click', '.edit-button', function() {
+    var $wrapper = $(this).parent();
+    var $updateform = $wrapper.find('#update-contribution');
+
+    if ($updateform.hasClass('active')) {
+      $wrapper.css({
+        backgroundColor: 'transparent',
+        zIndex: 99
+      });
+      $updateform.css({
+        opacity: 0
+      }).removeClass('active');
+    } else {
+      $wrapper.css({
+        backgroundColor: '#eee',
+        zIndex: 101
+      });
+      $updateform.addClass('active').css({
+        opacity: 1
+      });
+    }
+  });
+
+  /**
+   * 更新
+   */
+  _document.on('submit', '#update-contribution', function(event) {
+    event.preventDefault();
+    var _this = $(this);
+    updatedata = {
+      id: _this.find('.update-id').val(),
+      username: _this.find('.update-content--username').val(),
+      title: _this.find('.update-content--title').val(),
+      content: _this.find('.update-content--text').val().replace(/<br>/g, '\\n')
+    }
+    var jsondata = {
+      id: updatedata.id.json_escape(),
+      username: updatedata.username.json_escape(),
+      title: updatedata.title.json_escape(),
+      content: updatedata.content.json_escape()
+    }
+
+    $.ajax({
+      url: '/api/admin_update',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(jsondata),
+    })
+    .done(function() {
+      console.log("success");
+      applyUpdate();
+    })
+    .fail(function() {
+      console.log("error");
+      noticeError("投稿の更新");
+    });
+
+  });
 
   /**
    * 削除
@@ -47,6 +110,21 @@
         opacity: 0
       }).removeClass('active');
     }, 3000);
+  }
+
+  var applyUpdate = function() {
+    $('.contribution-edit').css({
+      backgroundColor: 'transparent',
+      zIndex: 99
+    });
+    $('#update-contribution').css({
+      opacity: 0
+    }).removeClass('active');
+
+    console.log(updatedata);
+    $('.user--name').first().text(updatedata.username);
+    $('.body--title').first().text(updatedata.title);
+    $('.body--content').first().text(updatedata.content);
   }
 
   /**
