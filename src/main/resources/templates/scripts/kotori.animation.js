5625070f0eb976dca91c7185191f3b7b48067107
nagisio <mhmsynasc@gmail.com>
2015-08-18 19:38:17 +0900

    Refactroing: Modified js files.

--- src/main/resources/templates/scripts/kotori.animation.js
+++ src/main/resources/templates/scripts/kotori.animation.js
@@ -1,90 +1,12 @@
 ;(function($) {
-  var _document = $(document);
-  var $posts = $('#post');
+  'use strict';
+
+  var $post = $('#post');
   var $search = $('#search');
 
   /**
-   * 新規投稿ボタンをクリックした時のイベント
+   * ヘッダーメニューの開閉を行う
    */
-  _document.on('click', '.btn--write', function() {
-    toggleHeader($posts, $search);
-  });
-
-  /**
-   * 検索ボタンをクリックした時のイベント
-   */
-  _document.on('click', '.hc--search', function() {
-    toggleHeader($search, $posts);
-  });
-
-  /**
-   * 設定ボタンをクリックした時のイベント
-   */
-  _document.on('click', '.hc--setting', function() {
-    var $details = $('.user-button-details');
-
-    if ($details.hasClass('active')) {
-      $details.removeClass('active').css({
-        top: 0,
-        opacity: 0
-      });
-    } else {
-      $details.addClass('active').css({
-        top: '60px',
-        opacity: 1
-      });
-    }
-  });
-  _document.on('click', '.main-circle', function() {
-    var $details = $('.user-button-details');
-    var $settings = $('.settings');
-
-    if ($settings.hasClass('active')) {
-      $settings.removeClass('active').css({
-        top: '-70px',
-        opacity: 0
-      });
-    } else {
-      if (!$details.hasClass('active')) return false;
-      $settings.addClass('active').css({
-        top: 0,
-        opacity: 1
-      });
-    }
-  });
-
-  /**
-   * 閉じるボタンをクリックした時のイベント
-   */
-  _document.on('click', '.close-button', function() {
-    var $settings = $('.settings');
-    $settings.removeClass('active').css({
-      top: '-70px',
-      opacity: 0
-    });
-  })
-
-  /**
-   * マウスホバー時のテキスト表示
-   */
-  _document.on('mouseenter', '.main-circle', function() {
-    var $buttonMenu = $(this).find('.button-menu');
-    $buttonMenu.css({opacity: 1});
-  });
-  _document.on('mouseleave', '.main-circle', function() {
-    var $buttonMenu = $(this).find('.button-menu');
-    $buttonMenu.css({opacity: 0});
-  });
-
-  /**
-   * 削除ボタンをクリックした時のイベント
-   */
-  _document.on('click', '.del-guide', function() {
-    var wrap = $(this).parent().find('.del-wrap').get(0);
-
-    $(wrap).slideToggle(400);
-  });
-
   var toggleHeader = function($current, $target) {
     if ($current.hasClass('active')) {
       slOff($current);
@@ -102,5 +24,27 @@
       $cur.removeClass('active').slideUp(400);
     }
   }
+
+  /**
+   * 新規投稿ボタンをクリックした時のイベント
+   */
+  $('.btn--write').on('click', function() {
+    toggleHeader($post, $search);
+  });
+
+  /**
+   * 検索ボタンをクリックした時のイベント
+   */
+  $('.btn--search').on('click', function() {
+    toggleHeader($search, $post);
+  });
+
+  /**
+   * 削除ボタンをクリックした時のイベント
+   */
+  $('.delete-guide').on('click', function() {
+    var wrap = $(this).parent().find('.delete-wrapper').get(0);
+    $(wrap).slideToggle(400);
+  });
 
 }(jQuery));
---
+++ src/main/resources/templates/scripts/kotori.main.js
@@ -0,0 +1,71 @@
+;(function($) {
+  'use strict';
+
+  var $document = $(document);
+  var $notice = $('.notice--error');
+
+  /**
+   * サーバからのjsonレスポンスをDOMに反映する
+   */
+  var createContribution = function(data) {
+    var contribution = '<div class="contribution"><div class="contribution__user cf"><div class="contribution__user--icon">icon</div><div class="contribution__user--name">'+data.username+'</div></div><div class="contribution__body"><div class="contribution__body--title">'+data.title+'</div><div class="contribution__body--content">'+data.content+'</div></div><div class="contribution__footer">';
+    if (data.isNew === true) {
+      contribution += '<span class="contribution__footer--new">New</span>';
+    }
+    contribution += ' '+data.editedCreatedTime+'</div></div>';
+    return contribution;
+  };
+
+  /**
+   * 投稿
+   */
+  $('#post-contribution').on('submit', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    $this.kotoriAjax({
+      url: '/api/post'
+    })
+    .done(function(data) {
+      $('#post').slideToggle(400, function() {
+        $this.find('input:not(.post-submit), textarea').val("");
+      });
+      $('#contributions').prepend($(createContribution(data)).fadeIn(400));
+    })
+    .fail(function(data) {
+      var msg = data.responseText || "新規投稿に失敗しました";
+      $notice.showMsg(msg);
+    });
+  });
+
+  /**
+   * 検索
+   */
+  $('#search-contribution').on('submit', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var keyword = $this.find('.search-keyword').val();
+    $(location).attr('href', '/search?q[keyword]=' + keyword);
+  });
+
+  /**
+   * 削除
+   */
+  $document.on('submit', '#delete-contribution', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    $this.kotoriAjax({
+      url: '/api/delete'
+    })
+    .done(function() {
+      $this.parents('.contribution').fadeOut(400, function() {
+        $(this).remove()
+      });
+    })
+    .fail(function() {
+      var msg = "投稿の削除に失敗しました";
+      $notice.showMsg(msg);
+      $this.find('.delete-key').val("");
+    });
+  });
+
+}(jQuery));
