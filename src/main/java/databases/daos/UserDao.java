6af402fcf57859128a016eef447640a6ae61c417
nagisio <mhmsynasc@gmail.com>
2015-08-21 18:42:01 +0900

    UserエンティティとDaoインタフェースの作成

---
+++ src/main/java/databases/daos/UserDao.java
@@ -0,0 +1,18 @@
+package databases.daos;
+
+import bulletinBoard.DBConfig;
+import databases.entities.User;
+import org.seasar.doma.Dao;
+import org.seasar.doma.Delete;
+import org.seasar.doma.Insert;
+
+@Dao(config = DBConfig.class)
+public interface UserDao {
+
+    @Insert
+    int insert(User user);
+
+    @Delete
+    int delete(User user);
+
+}
---
+++ src/main/java/databases/entities/User.java
@@ -0,0 +1,38 @@
+package databases.entities;
+
+import org.seasar.doma.*;
+import org.seasar.doma.jdbc.entity.NamingType;
+
+@Entity(naming = NamingType.LOWER_CASE)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name ="userid")
+    private String userid;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+}
