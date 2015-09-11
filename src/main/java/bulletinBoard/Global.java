a7e7a48db961e7f30f222a8e0571f427af566dd9
nagisio <mhmsynasc@gmail.com>
2015-09-11 18:37:15 +0900

    Fix: コマンドライン引数が無い場合にサーバが設定されない問題を修正

--- src/main/java/bulletinBoard/Global.java
+++ src/main/java/bulletinBoard/Global.java
@@ -11,15 +11,13 @@ public class Global {
         RedisServer.getRedisServer();
         ApplicationRoute application = ApplicationRoute.getApplicationRoute();
 
-        Stream.of(args).findFirst().ifPresent(arg -> {
-            try {
-                application.initServerPort(Integer.valueOf(arg));
-            } catch (IllegalArgumentException e) {
-                application.initServerPort(9000);
-            } finally {
-                application.initServerRoutes();
-            }
-        });
+        try {
+            Stream.of(args).findFirst().ifPresent(arg -> application.initServerPort(Integer.valueOf(arg)));
+        } catch (IllegalArgumentException e) {
+            application.initServerPort(9000);
+        } finally {
+            application.initServerRoutes();
+        }
 
     }
 
